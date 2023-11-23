640861c8a333df7393586d5f74ab504dd47148ba
unknown <fraolgetachew2727@gmail.com>
2023-11-23 14:14:24 +0300

    Docker initialization with node js

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,30 @@
 import express, { Request, Response } from 'express';
 const app = express();
-
 // database connections
+import mysql from "mysql2";
+// Create a connection pool
+const pool = mysql.createPool({
+  host: "",
+  user: "admin",
+  password: '',
+  database: "lersha_dev",
+  port:3306
+});
+
+// Check the database connection
+pool.getConnection((err, connection) => {
+  if (err) {
+    console.error('Error connecting to the database:', err);
+    return;
+  }
+
+  console.log('Connected to the database!');
+  
+  // Release the connection
+  connection.release();
+});
+let query=""
+// Close the connection pool after 5 seconds
 
 // routes
 app.get('/', (req: Request, res: Response) => {
@@ -11,7 +34,6 @@ app.get('/', (req: Request, res: Response) => {
 // some more stuff
 
 const APP_PORT = 2007;
-
 app.listen(APP_PORT, () => {
   console.log(`Server started on port ${APP_PORT}`);
 });
